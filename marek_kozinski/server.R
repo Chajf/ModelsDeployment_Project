84b8c15640d38e728e8525472e8727e493ac9bb5
Michal <Michciak17@gmail.com>
2024-04-23 23:58:55 +0200

    margins and paddings v0.9

--- marek_kozinski/server.R
+++ marek_kozinski/server.R
@@ -1,12 +1,3 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
 library(DT)
 
@@ -37,21 +28,19 @@ function(input, output, session) {
            main = 'Histogram of waiting times')
 
   })
-  
-  output$distPlot2 <- renderPlot({
-      
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins2 + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white',
-         xlab = 'Waiting time to next eruption (in mins)',
-         main = 'Histogram of waiting times')
-      
-  })
-    
-  output$table <- renderTable(iris[c(1,2),c(1,2)], options = list(scrollX=TRUE))
+  # 
+  # output$distPlot2 <- renderPlot({
+  #     
+  #   # generate bins based on input$bins from ui.R
+  #   x    <- faithful[, 2]
+  #   bins <- seq(min(x), max(x), length.out = input$bins2 + 1)
+  #   
+  #   # draw the histogram with the specified number of bins
+  #   hist(x, breaks = bins, col = 'darkgray', border = 'white',
+  #        xlab = 'Waiting time to next eruption (in mins)',
+  #        main = 'Histogram of waiting times')
+  #     
+  # })
   
   modifiedData <- reactiveValues(df = NULL)
   
@@ -97,4 +86,6 @@ function(input, output, session) {
       })
     })
   })
+  
+  
 }
--- marek_kozinski/ui.R
+++ marek_kozinski/ui.R
@@ -1,160 +1,82 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
 library(bslib)
 library(DT)
 
 thematic::thematic_shiny()
 
-# Define UI for application that draws a histogram
-# page_fluid(
-#   # Application title
-#   # titlePanel("Visual Model bUIlding"),
-  # theme = bs_theme(
-  #   bg = "#101010",
-  #   fg = "#FFF",
-  #   primary = "#E69F00",
-  #   secondary = "#0072B2",
-  #   success = "#009E73",
-  #   base_font = font_google("Inter"),
-  #   code_font = font_google("JetBrains Mono")
-  # ),
-#   lang = "en",
-#   titlePanel(
-#     HTML("VISUAL MODEL B<span style='font-size:150%; font-weight:bold;'>[UI]</span>LDING")
-#   ),
-#   # Sidebar with a slider input for number of bins
-#   layout_sidebar(
-#     sidebar = sidebarPanel(
-#             titlePanel(
-#               HTML("Sidebar")
-#             ),
-#             sliderInput("bins",
-#                         "Number of bins:",
-#                         min = 1,
-#                         max = 50,
-#                         value = 30
-#               ),
-#             # sliderInput("bins",
-#             #             "Number of bins:",
-#             #             min = 1,
-#             #             max = 50,
-#             #             value = 30
-#             #   ),
-#             width = 12,
-#             ),
-#     splitLayout(
-#       mainPanel(
-#         navset_underline(
-#           # title = "",
-#           nav_panel(
-#             title = "cokolwiek",
-#             column(plotOutput("distPlot"),
-#                    width = 6),
-#             column(titlePanel(HTML("Sidebar")),
-#                    width = 6)
-#           ),
-#           nav_panel(
-#             title = "cokolwiek2",
-#             #content
-#           ),
-#         ),
-#         width = 12,
-#       )
-#     )
-#   )
-# )
-
+#===============================================================================
+# Page 1
 #===============================================================================
 
-# first page
-# page1 <- page_fillable(
-#   theme = bs_theme(preset = "pulse"),
-#   layout_sidebar(
-#     sidebar = sidebar(
-#       sliderInput("bins2",
-#                   "Number of bins:",
-#                   min = 1,
-#                   max = 50,
-#                   value = 30
-#       ),
-#       width = 300,
-#       open = "always",
-#     ),
-#     plotOutput("distPlot2")
-#   )
-# )
+# Tab 1
 
-# page2 <- page_fillable(
-#   layout_columns(
-#     card(titlePanel("Options"),
-#          sliderInput("bins",
-#                        "Number of bins:",
-#                        min = 1,
-#                        max = 50,
-#                        value = 30
-#     )),
-#     card(plotOutput("distPlot")),
-#     col_widths = c(2,10)
-#   ),
-#   theme = bs_theme(
-#     preset = "pulse"
-#   )
-# )
+tab1 <- fluidPage(
+  layout_sidebar(
+    sidebar = sidebar(
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30
+      ),
+      width = 300,
+      open = "open",
+    ),
+    plotOutput("distPlot")
+  )
+)
 
-page1 <- page_fillable(
-  navset_card_tab(
-    title = "Histograms by species",
-    nav_panel("jeden", 
-              p(
-                layout_sidebar(
-                  sidebar = sidebar(
-                    sliderInput("bins2",
-                                "Number of bins:",
-                                min = 1,
-                                max = 50,
-                                value = 30
-                    ),
-                    width = 300,
-                    open = "always",
-                  ),
-                  plotOutput("distPlot2")
-                )
-              )),
-    nav_panel("dwa", "content"),
-    nav_panel("trzy", "content")
+
+
+# Tab 2
+
+tab2 <- "place_holder"
+
+
+
+# Tab 3
+
+tab3 <- "place_holder"
+
+
+
+# Page
+
+exploration <- page_fillable(
+  navset_card_underline(
+    title = "Data Exploration",
+    nav_panel("Imputation", tab1), # data imputation
+    nav_panel("Variables", tab2), # data description, variable statistics
+    nav_panel("Visualisation", tab3) # distributions, boxplots, dependecies 
   ),
   theme = bs_theme(
     preset = "pulse"
   )
 )
+
+#===============================================================================
+# Page 2
+#===============================================================================
 
 page2 <- page_fillable(
   navset_card_underline(
     title = "Histograms by species",
-    nav_panel("jeden", 
-              p(
-                layout_sidebar(
-                  sidebar = sidebar(
-                    sliderInput("bins",
-                                "Number of bins:",
-                                min = 1,
-                                max = 50,
-                                value = 30
-                    ),
-                    width = 300,
-                    open = "always",
-                  ),
-                  plotOutput("distPlot")
-                )
-              )),
+    # nav_panel("jeden", 
+    #           fluidPage(
+    #             layout_sidebar(
+    #               sidebar = sidebar(
+    #                 sliderInput("bins",
+    #                             "Number of bins:",
+    #                             min = 1,
+    #                             max = 50,
+    #                             value = 30
+    #                 ),
+    #                 width = 300,
+    #                 open = "always",
+    #               ),
+    #               plotOutput("distPlot")
+    #             )
+    #           )),
     nav_panel("dwa", "content"),
     nav_panel("trzy", "content")
   ),
@@ -163,22 +85,16 @@ page2 <- page_fillable(
   )
 )
 
+#===============================================================================
+# Sidebar
+#===============================================================================
+
 sidebar_main <- sidebar(
-  # tags$head(
-  #   tags$style(
-  #     HTML("
-  #       .sidebar {
-  #         position: fixed;
-  #         # top: 0;
-  #         # left: 0;
-  #         # bottom: 0;
-  #         # width: 100px; /* Adjust as needed */
-  #         background-color: #f8f9fa; /* Sidebar background color */
-  #         box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); /* Optional shadow effect */
-  #       }
-  #     ")
-  #   )
-  # ),
+  tags$style(HTML("
+                  .card-body {padding: 0px;} 
+                  .container-fluid{padding-left: 0px;} 
+                  .bslib-sidebar-layout>.main{padding: 0px;}
+                  ")),
   titlePanel(HTML("Upload file")),
   fileInput("upload", "Upload a .csv/.tsv file", accept = c(".csv",".tsv"), multiple = FALSE),
   selectInput("sep", "Choose the separator:",
@@ -192,40 +108,19 @@ sidebar_main <- sidebar(
   uiOutput("varSelect"),
 )
 
-# bs_global_add_rules(HTML(".navbar .navbar-default .navbar-static-top {margin-bottom: 0px}"))
-# page combined 
+#===============================================================================
+# Combine
+#===============================================================================
+
 page_navbar(
-  # tags$head(tags$style(HTML(".navbar .navbar-default .navbar-static-top {margin-bottom: 0px}"))),
-  # tags$style(".content-wrapper{margin-left: 0px;}"),
-  title = HTML("<ins>VISUAL MODEL </ins></br>&emsp;<ins>B</ins><span style='font-size:150%; font-weight:bold;'>[UI]</span><ins>LDING</ins>&emsp;&emsp;"),
-  # tym steruje się tematem we wszsytkich page'ach
-  # osobno w np. page2 nie zmienia sie
+  title = HTML("&emsp;<ins>VISUAL MODEL </ins></br>&emsp;<ins>B</ins><span style='font-size:150%; font-weight:bold;'>[UI]</span><ins>LDING</ins>&emsp;"),
   theme = bs_theme(
-    # bg = "black",
-    # fg = "white",
-    # primary = "#E69F00",
-    # secondary = "#0072B2",
-    # success = "#009E73",
-    # base_font = font_google("Inter"),
-    # code_font = font_google("JetBrains Mono")
     preset = "pulse",
   ),
   underline = TRUE,
   sidebar = sidebar_main,
-  # sidebar = sidebarPanel(
-  #     card(titlePanel(
-  #       HTML("Sidebar")
-  #     )),
-  #     card(fileInput("upload", "Upload a file")),
-  #     card(sliderInput("bins3",
-  #                      "Number of bins:",
-  #                      min = 1,
-  #                      max = 50,
-  #                      value = 30
-  #     )),
-  #     width = 12
-  # ),
-  nav_panel(title = HTML("<span style='font-size:80%; font-weight:bold;'>DATA EXPLORATION</span>"), page1),
+  padding = 0,
+  nav_panel(title = HTML("<span style='font-size:80%; font-weight:bold;'>DATA EXPLORATION</span>"), exploration),
   nav_panel(title = HTML("<span style='font-size:80%; font-weight:bold;'>MODELLING & RESULTS</span>"), page2)
 )
 
