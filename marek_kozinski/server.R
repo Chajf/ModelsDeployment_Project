bfcc5080132ec8bdabc54f7588d61061f9582127
Chajf <patrykmarek8@gmail.com>
2024-04-24 19:01:41 +0200

    Preprocessing UI

--- marek_kozinski/server.R
+++ marek_kozinski/server.R
@@ -1,14 +1,6 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
 library(DT)
+library(bsicons)
 
 # Define server logic required to draw a histogram
 function(input, output, session) {
@@ -37,21 +29,19 @@ function(input, output, session) {
            main = 'Histogram of waiting times')
 
   })
-  
-  output$distPlot2 <- renderPlot({
-      
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins2 + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white',
-         xlab = 'Waiting time to next eruption (in mins)',
-         main = 'Histogram of waiting times')
-      
-  })
-    
-  output$table <- renderTable(iris[c(1,2),c(1,2)], options = list(scrollX=TRUE))
+  # 
+  # output$distPlot2 <- renderPlot({
+  #     
+  #   # generate bins based on input$bins from ui.R
+  #   x    <- faithful[, 2]
+  #   bins <- seq(min(x), max(x), length.out = input$bins2 + 1)
+  #   
+  #   # draw the histogram with the specified number of bins
+  #   hist(x, breaks = bins, col = 'darkgray', border = 'white',
+  #        xlab = 'Waiting time to next eruption (in mins)',
+  #        main = 'Histogram of waiting times')
+  #     
+  # })
   
   modifiedData <- reactiveValues(df = NULL)
   
@@ -95,6 +85,53 @@ function(input, output, session) {
           })
         })
       })
+    })
+  })
+  
+  output$target_var_ui <- renderUI({
+    selectInput("target_var", "Choose target variable", choices = names(modifiedData$df))
+  })
+  
+  output$v_box <- renderUI({
+    value_box(title = "Target variable",
+              value = input$target_var)
+  })
+  
+  output$type <- renderUI({
+    col <- input$target_var
+    type <- ifelse(sapply(modifiedData$df[col],class)=="numeric","Regression","Classification")
+    icon <- ifelse(type=="Regression","graph-up","bullseye")
+    value_box(title = "Type of task",
+              value = type,
+              showcase = bs_icon(icon))
+  })
+  
+  output$inputs <- renderUI({
+    req(input$preproc)
+    lapply(input$preproc, function(i) {
+      if (i == "PCA") {
+        sliderInput("pca_comp",
+                    "Number of components",
+                    min = 2,
+                    max = ncol(modifiedData$df),
+                    value = round(ncol(modifiedData$df)/2),
+                    step = 1
+        )
+      } else if (i == "Class other") {
+        sliderInput("other",
+                    "Threshold",
+                    min = 0.01,
+                    max = 0.95,
+                    value = 0.15
+        )
+      } else if (i %in% c("Logarythm transform", "Square root transform")) {
+        numeric_vars <- names(modifiedData$df)[sapply(modifiedData$df, is.numeric)]
+        selectizeInput(paste0("select_", gsub(" ", "_", i)),
+                       paste0("Select variables for ", i),
+                       choices = numeric_vars,
+                       multiple = TRUE
+        )
+      }
     })
   })
 }
--- marek_kozinski/ui.R
+++ marek_kozinski/ui.R
@@ -1,73 +1,103 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
 library(bslib)
 library(DT)
 
 thematic::thematic_shiny()
 
-page1 <- page_fillable(
-  navset_card_tab(
-    title = "Histograms by species",
-    nav_panel("jeden", 
-              p(
-                layout_sidebar(
-                  sidebar = sidebar(
-                    sliderInput("bins2",
-                                "Number of bins:",
-                                min = 1,
-                                max = 50,
-                                value = 30
-                    ),
-                    width = 300,
-                    open = "always",
-                  ),
-                  plotOutput("distPlot2")
-                )
-              )),
-    nav_panel("dwa", "content"),
-    nav_panel("trzy", "content")
+#===============================================================================
+# Page 1
+#===============================================================================
+
+# Tab 1
+
+tab1 <- fluidPage(
+  layout_sidebar(
+    sidebar = sidebar(
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30
+      ),
+      width = 300,
+      open = "open",
+    ),
+    plotOutput("distPlot")
+  )
+)
+
+
+
+# Tab 2
+
+tab2 <- "place_holder"
+
+
+
+# Tab 3
+
+tab3 <- "place_holder"
+
+
+
+# Page
+
+exploration <- page_fillable(
+  navset_card_underline(
+    title = HTML("<span style='font-size:100%; font-weight:bold;'>Data Exploration</span>"),
+    nav_panel("Imputation", tab1), # data imputation
+    nav_panel("Variables", tab2), # data description, variable statistics
+    nav_panel("Visualisation", tab3) # distributions, boxplots, dependecies 
   ),
   theme = bs_theme(
     preset = "pulse"
+  )
+)
+
+#===============================================================================
+# Page 2
+#===============================================================================
+
+tab_m1 <- fluidPage(
+  layout_sidebar(
+    sidebar = sidebar(
+      titlePanel("Model target"),
+      uiOutput("target_var_ui"),
+      checkboxGroupInput("preproc",
+                         "Steps to add",
+                         choices = c("Normalize","Dummy variables","PCA",
+                                     "Remove zero variance", "Remove near zero variance",
+                                     "Logarythm transform", "Class other", "Square root transform")),
+      uiOutput("inputs"),
+      width = 300,
+      open = "open",
+    ),
+    uiOutput("v_box"),
+    uiOutput("type")
   )
 )
 
 page2 <- page_fillable(
   navset_card_underline(
-    title = "Histograms by species",
-    nav_panel("jeden", 
-              p(
-                layout_sidebar(
-                  sidebar = sidebar(
-                    sliderInput("bins",
-                                "Number of bins:",
-                                min = 1,
-                                max = 50,
-                                value = 30
-                    ),
-                    width = 300,
-                    open = "always",
-                  ),
-                  plotOutput("distPlot")
-                )
-              )),
-    nav_panel("dwa", "content"),
-    nav_panel("trzy", "content")
+    title = "Model Building",
+    nav_panel("Building", tab_m1),
+    nav_panel("Results", "content")
   ),
   theme = bs_theme(
     preset = "pulse"
   )
 )
 
+#===============================================================================
+# Sidebar
+#===============================================================================
+
 sidebar_main <- sidebar(
+  tags$style(HTML("
+                  .card-body {padding: 0px;} 
+                  .container-fluid{padding-left: 0px;} 
+                  .bslib-sidebar-layout>.main{padding: 0px;}
+                  ")),
   titlePanel(HTML("Upload file")),
   fileInput("upload", "Upload a .csv/.tsv file", accept = c(".csv",".tsv"), multiple = FALSE),
   selectInput("sep", "Choose the separator:",
@@ -81,15 +111,20 @@ sidebar_main <- sidebar(
   uiOutput("varSelect"),
 )
 
+#===============================================================================
+# Combine
+#===============================================================================
+
 page_navbar(
-  title = HTML("<ins>VISUAL MODEL </ins></br>&emsp;<ins>B</ins><span style='font-size:150%; font-weight:bold;'>[UI]</span><ins>LDING</ins>&emsp;&emsp;"),
+  title = HTML("&emsp;<ins>VISUAL MODEL </ins></br>&emsp;<ins>B</ins><span style='font-size:150%; font-weight:bold;'>[UI]</span><ins>LDING</ins>&emsp;"),
   theme = bs_theme(
     preset = "pulse",
   ),
   underline = TRUE,
   sidebar = sidebar_main,
-  nav_panel(title = HTML("<span style='font-size:80%; font-weight:bold;'>DATA EXPLORATION</span>"), page1),
-  nav_panel(title = HTML("<span style='font-size:80%; font-weight:bold;'>MODELLING & RESULTS</span>"), page2)
+  padding = 0,
+  nav_panel(title = HTML("<span style='font-size:100%; font-weight:bold;'>DATA EXPLORATION</span>"), exploration),
+  nav_panel(title = HTML("<span style='font-size:100%; font-weight:bold;'>MODELLING & RESULTS</span>"), page2)
 )
 
 
